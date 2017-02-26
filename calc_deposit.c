197cd18f9ca6fc1a2899985f49f3eb7da7ad6928
vbntv <vbuntovv@mail.ru>
2017-02-27 02:11:18 +0700

    Scan vklad and srok

--- calc_deposit.c
+++ calc_deposit.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() 
+{
+    int srok;
+    float vklad;
+
+    printf("Срок: ");        
+    scanf("%d", &srok);
+    printf("Сумма вклада: ");
+    scanf("%f", &vklad);
